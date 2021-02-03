db1a4a2ebb426b49e16625cab8c59caed7baf36c
darkgod1802 <bryan.choque.canaza@gmail.com>
2021-02-03 14:36:43 -0400

    Create repository

---
+++ lib/core.dart
@@ -0,0 +1,9 @@
+library core;
+
+import 'package:flutter/material.dart';
+
+export './src/slider_controller.dart';
+
+part './src/license.dart';
+part './src/calendar/calendar_helper.dart';
+part './src/calendar/hijri_date_time.dart';
